ad0d0d7853835c0934764dc34e9e9f5032b6dcf8
zearkiatos <caprilespe@outlook.com>
2021-07-23 22:11:40 -0400

    Assign and Aritmetica

--- index.php
+++ index.php
@@ -1,15 +1,21 @@
 <?php
-/*
-    Comment in various lines
-*/
-    class Answer 
-    {
-        /**
-         * Coment of various lines
-         * lines for
-         * words
-         */
-        protected $clients = []; // My comment
 
-        protected $insurers = []; # My comment
-    }
+// Assign
+echo "Hello \n";
+$num = 9;
+$lang = ['es' => 'español', 'en' => 'english'];
+// Aritmetic
+
+echo "Sum 2 + 2" . ((int) 2 + (int) 2);
+
+echo "Substract 2 - 2" . (2 - 2);
+
+echo "Product 2 * 2" . (2 * 2);
+
+echo "Division 2 / 2" . 2 / 2;
+
+echo "Module 2 % 2" . 2 % 2;
+
+echo "Exp 2 ** 2" . 2 ** 2;
+
+?>
